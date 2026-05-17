277760c215cced0a469dd69515f07060b0f0cc56
Ivan Dolgopolov <fertexop@yandex.ru>
2026-05-17 16:16:58 +0700

    feat(root): Создал структуру проекта

---
+++ src/main/java/shift/lab/shift_lab_java_may_2026/ShiftLabJavaMay2026Application.java
@@ -0,0 +1,13 @@
+package shift.lab.shift_lab_java_may_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftLabJavaMay2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftLabJavaMay2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/shift/lab/shift_lab_java_may_2026/ShiftLabJavaMay2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package shift.lab.shift_lab_java_may_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftLabJavaMay2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
